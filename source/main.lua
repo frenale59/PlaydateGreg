218221e01ea5e2967ffbcc45cf910ecb61c556d6
frenale59 <72805883+frenale59@users.noreply.github.com>
2023-03-17 10:07:57 +0100

    Add files via upload

---
+++ source/main.lua
@@ -0,0 +1,9 @@
+import "CoreLibs/object"
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
+import "libraries/noble/Noble"
+import "libraries/animatedSprite/AnimatedSprite.lua"
+import "games/scenes/simpleScene.lua"
+
+Noble.new(SimpleScene)
